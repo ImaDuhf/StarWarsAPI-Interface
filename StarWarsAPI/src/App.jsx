066ffc832a40a30760b87556b36826dc6cf5ae95
ImaDuhf <timperson234@gmail.com>
2023-01-25 10:52:47 +0100

    A

--- StarWarsAPI/src/App.jsx
+++ StarWarsAPI/src/App.jsx
@@ -1,7 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-	return <div className="App"></div>;
+	// fetch('https://swapi.dev/api/people/')
+	// 		.then((resp) => resp.json())
+	// 		.then((data) => console.log(data));
+
+	const [option, setOption] = useState('people');
+
+	return (
+		<div className="App">
+			<h1>Star Wars API Interface</h1>
+		</div>
+	);
 }
 
 export default App;
